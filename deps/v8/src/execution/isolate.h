adb34a3f316deaff32a344620381b629c443163d
Brian Hackett <bhackett1024@gmail.com>
2026-03-09 22:01:02 +0000

    Add miscellaneous V8 recording support
    Patch isolate thread ownership, compilation cache DCHECKs, handle
    scope diagnostics, stack guard invalidation, and platform job
    notification guards for recording.

--- deps/v8/src/execution/isolate.h
+++ deps/v8/src/execution/isolate.h
@@ -740,6 +740,7 @@ class V8_EXPORT_PRIVATE Isolate final : private HiddenFactory {
   inline void set_pending_message(Object message_obj);
 
   THREAD_LOCAL_TOP_ADDRESS(Object, scheduled_exception)
+
   inline Object scheduled_exception();
   inline bool has_scheduled_exception();
   inline void clear_scheduled_exception();
@@ -1169,7 +1170,10 @@ class V8_EXPORT_PRIVATE Isolate final : private HiddenFactory {
     return descriptor_lookup_cache_;
   }
 
-  HandleScopeData* handle_scope_data() { return &handle_scope_data_; }
+  HandleScopeData* handle_scope_data() {
+    DCHECK(pthread_self() == owning_thread_);
+    return &handle_scope_data_;
+  }
 
   HandleScopeImplementer* handle_scope_implementer() const {
     DCHECK(handle_scope_implementer_);
@@ -1957,6 +1961,14 @@ class V8_EXPORT_PRIVATE Isolate final : private HiddenFactory {
   ReadOnlyHeap* read_only_heap_ = nullptr;
   std::shared_ptr<ReadOnlyArtifacts> artifacts_;
   std::unique_ptr<StringTable> string_table_;
+
+  // I don't know what isolate ownership rules are and they seem more
+  // complicated than is initially apparent. This is used for checking
+  // that various parts of the isolate are accessed in a single threaded
+  // manner.
+ public:
+  pthread_t owning_thread_;
+ private:
 
   const int id_;
   EntryStackItem* entry_stack_ = nullptr;
