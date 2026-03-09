08341fb0a14d1ab9907746aeb56f68f02dcb8c96
Brian Hackett <bhackett1024@gmail.com>
2026-03-09 19:50:43 +0000

    Add V8 crash diagnostics and misc recording support
    Add busy-wait on fatal errors for recording, crash reason tracking,
    recording-aware fatal paths, compilation cache fixes, message object
    bookmarks, handle scope diagnostics, isolate thread ownership checks,
    and platform job notification guards.

--- deps/v8/src/base/logging.cc
+++ deps/v8/src/base/logging.cc
@@ -12,6 +12,8 @@
 #include "src/base/debug/stack_trace.h"
 #include "src/base/platform/platform.h"
 
+#include <dlfcn.h>
+
 namespace v8 {
 namespace base {
 
@@ -134,6 +136,18 @@ class FailureMessage {
 
 }  // namespace
 
+const char* gCrashReason;
+
+extern "C" const char* V8RecordReplayCrashReasonCallback() {
+  return gCrashReason;
+}
+
+static __attribute__((noinline)) void BusyWait() {
+  fprintf(stderr, "Busy-waiting... (pid %d)", getpid());
+  volatile int x = 1;
+  while (x) {}
+}
+
 #ifdef DEBUG
 void V8_Fatal(const char* file, int line, const char* format, ...) {
 #else
@@ -141,6 +155,28 @@ void V8_Fatal(const char* format, ...) {
   const char* file = "";
   int line = 0;
 #endif
+
+  {
+    char str[1024];
+    va_list arguments;
+    va_start(arguments, format);
+    vsnprintf(str, sizeof(str), format, arguments);
+    str[sizeof(str) - 1] = 0;
+    va_end(arguments);
+
+    gCrashReason = strdup(str);
+  }
+
+  if (getenv("RECORD_REPLAY_WAIT_AT_CRASH") ||
+      getenv("RECORD_REPLAY_WAIT_AT_FATAL_ERROR")) {
+    BusyWait();
+  }
+
+  // Avoid problems getting backtraces when recording/replaying.
+  if (dlsym(RTLD_DEFAULT, "RecordReplayAttach")) {
+    *(volatile int*)0 = 0;
+  }
+
   va_list arguments;
   va_start(arguments, format);
   // Format the error message into a stack object for later retrieveal by the
@@ -164,6 +200,7 @@ void V8_Fatal(const char* format, ...) {
   if (v8::base::g_print_stack_trace) v8::base::g_print_stack_trace();
 
   fflush(stderr);
+
   v8::base::OS::Abort();
 }
 
