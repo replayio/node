43c31bed195ce1987b377effba0f8987da04af25
Domi <Domiii@users.noreply.github.com>
2026-09-23 00:21:20 +0800

    Skip streamed LookupScript under RR; streamed script cache diverges without top-level SFI support. (#145)

--- deps/v8/src/codegen/compiler.cc
+++ deps/v8/src/codegen/compiler.cc
@@ -3078,7 +3078,11 @@ Compiler::GetSharedFunctionInfoForStreamedScript(
   // Check if compile cache already holds the SFI, if so no need to finalize
   // the code compiled on the background thread.
   CompilationCache* compilation_cache = isolate->compilation_cache();
-  {
+
+  // For now we don't support using the compilation cache with streamed scripts,
+  // due to the lack of support for handling the case when the script is present
+  // but not the top level SFI. (Chromium feature: no-streamed-script-cache)
+  if (!recordreplay::IsRecordingOrReplaying()) {
     TRACE_EVENT0(TRACE_DISABLED_BY_DEFAULT("v8.compile"),
                  "V8.StreamingFinalization.CheckCache");
     maybe_result = compilation_cache->LookupScript(source, script_details,
